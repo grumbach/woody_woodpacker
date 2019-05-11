eb73bd4e9f9296fec7763e6abc4300ea75fdbb14
grumbach <agrumbac@student.42.fr>
2019-05-12 01:11:48 +0200

    [ADD] graceful memory layout drawing

--- srcs/elf64/elf64_setup_payload.c
+++ srcs/elf64/elf64_setup_payload.c
@@ -6,12 +6,46 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/11 00:10:33 by agrumbac          #+#    #+#             */
-/*   Updated: 2019/05/11 11:25:48 by agrumbac         ###   ########.fr       */
+/*   Updated: 2019/05/12 01:10:16 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "elf64_private.h"
 #include <time.h>
+
+/*
+**  Elf64_packer memory overview
+**  ============================
+**
+**         | mem address | file offset | payload rel address      |
+**         | ----------- | ----------- | ------------------------ |
+**         | p_vaddr     | p_offset    | relative_pt_load_address |
+**         |             |             |                          |
+**         | sh_addr     | sh_offset   | relative_text_address    |
+**         |             |             |                          |
+**         | e_entry     |             | relative_entry_address   |
+**
+**                          .        .
+**                          |  ...   |
+**              p_offset -> |========| PT_LOAD  - relative_pt_load_address
+**                          |  ...   |          ^
+**                          |  ...   |          |
+**             sh_offset -> |--------| .Text    |- relative_text_address
+**                      |   |  ...   |          |^
+**                      V   |  ...   |          ||
+**    offset_in_section -   |>>>>>>>>| entry    ||- relative_entry_address
+**                          |  ...   |          ||^
+**                          |  ...   |          |||
+**                          |--------|          |||
+**    section_end_offset -> |@@@@@@@@| payload  |||
+**                          |@      @| |
+**                          |@      @| V
+**                          |@@@@@@@@| payload_size
+**                          |  ...   |
+**                          |========|
+**                          |  ...   |
+**                          .        .
+*/
 
 # define CALL_INSTR_SIZE	5 /* sizeof "call mark_below" -> e8 2000 0000 */
 # define SECRET_SIGNATURE	"42Remblai"
@@ -40,9 +74,9 @@ static bool	init_constants(struct payload_constants *constants, \
 	memcpy(constants->key, SECRET_SIGNATURE, SECRET_LEN);
 	generate_key((char *)constants->key + SECRET_LEN, 16 - SECRET_LEN);
 
-	constants->relative_pt_load_address = original_entry->section_end_offset - endian_8(original_entry->safe_phdr->p_offset);
+	constants->relative_pt_load_address = original_entry->section_end_offset - endian_8(original_entry->safe_phdr->p_offset); // TODO check with v_addresses instead of offsets
 	constants->pt_load_size             = endian_8(original_entry->safe_phdr->p_memsz);
-	constants->relative_text_address    = original_entry->section_end_offset - endian_8(original_entry->safe_shdr->sh_offset);
+	constants->relative_text_address    = endian_8(original_entry->safe_shdr->sh_size);
 	constants->relative_entry_address   = constants->relative_text_address - original_entry->offset_in_section;
 
 	return true;
@@ -59,13 +93,16 @@ bool		setup_payload(void *clone, const struct entry *original_entry)
 	const size_t	text_size    = constants.relative_text_address;
 	void	*payload_location    = clone + original_entry->section_end_offset;
 	void	*constants_location  = payload_location + CALL_INSTR_SIZE;
-	void	*text_location       = payload_location - text_size;
-
-	memcpy(text_location - original_entry->offset_in_section, begin_payload, payload_size);//TMP WTF
+	void	*text_location       = payload_location - constants.relative_text_address;
 
 	// encrypt(32, text_location, constants.key, text_size);
 	// memcpy(payload_location, begin_payload, payload_size);
 	// memcpy(constants_location, &constants, sizeof(constants));
+
+
+	const size_t	e_offset = original_entry->safe_shdr->sh_offset + original_entry->offset_in_section;
+	void	*entry_location  = clone + e_offset;
+	memcpy(entry_location, begin_payload, payload_size);
 
 	return true;
 }
