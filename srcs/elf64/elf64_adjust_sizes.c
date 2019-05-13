3bdd4519e2957c23ac7a652ddb7fac7f9670827d
Jonathan FORTIN <jfortin@student.42.fr>
2019-05-13 17:10:16 +0200

    [FIX] adjust_sizes

--- srcs/elf64/elf64_adjust_sizes.c
+++ srcs/elf64/elf64_adjust_sizes.c
@@ -6,7 +6,7 @@
 /*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 16:20:29 by jfortin           #+#    #+#             */
-/*   Updated: 2019/05/13 16:35:24 by jfortin          ###   ########.fr       */
+/*   Updated: 2019/05/13 17:06:54 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static bool	adjust_sizes(shift_amount)
 {
 	struct entry	clone_entry;
 
-	if (find_entry(&clone_entry, clone_safe))
+	if (!find_entry(&clone_entry, clone_safe))
 		return (errors(ERR_THROW, "adjust_sizes"));
 	clone_entry.safe_shdr += shift_amount;
 	clone_entry.safe_phdr += shift_amount;
